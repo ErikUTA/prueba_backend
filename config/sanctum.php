2b9a4952642224e182a0fac85b62ae35d966c8ba
erikUTA <erik4tohj@gmail.com>
2025-03-12 15:26:42 -0600

    updates

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,web-production-93e2a.up.railway.app')),
 
     /*
     |--------------------------------------------------------------------------
